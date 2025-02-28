ef5a0c6bb0d61825c30e84e9423ca31900699d2c
Moi-glaza-vezde <moi.glaza.vezde@gmail.com>
2025-02-28 10:34:09 +0500

    done hm5

--- src/s2-homeworks/hw02/HW2.tsx
+++ src/s2-homeworks/hw02/HW2.tsx
@@ -70,7 +70,7 @@ function HW2() {
     }
 
     return (
-        <div id={'hw2'}>
+        <div className={s2.container} id={'hw2'}>
             <div className={s2.hwTitle}>Homework #2</div>
             <div className={s2.hw}>
                 <Affairs
