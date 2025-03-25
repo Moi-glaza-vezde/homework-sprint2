16e54eb0c29fb2f86d95d91d4e929d30d20aba19
Moi-glaza-vezde <moi.glaza.vezde@gmail.com>
2025-03-25 12:01:09 +0500

    hw9

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -65,7 +65,7 @@ function Clock() {
     const monthNAme = months[date.getMonth()];
 
 
-    const stringTime = `${format(hours)}:${format(minutes)}:${format(seconds)}` || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
+    const stringTime = `${format(hours)}:${format(minutes)}` || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
     const stringDate = `${day}.${format(month)}.${year}` || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
@@ -104,7 +104,7 @@ function Clock() {
             <div className={s.buttonsContainer}>
                 <SuperButton
                     id={'hw9-button-start'}
-                    disabled={intervalID.current !== null} // пишут студенты // задизэйблить если таймер запущен
+                    disabled={isRunning} // пишут студенты // задизэйблить если таймер запущен
                     onClick={start}
                     xType={'default'}
                 >
@@ -112,7 +112,7 @@ function Clock() {
                 </SuperButton>
                 <SuperButton
                     id={'hw9-button-stop'}
-                    disabled={intervalID.current == null} // пишут студенты // задизэйблить если таймер не запущен
+                    disabled={!isRunning} // пишут студенты // задизэйблить если таймер не запущен
                     onClick={stop}
                     xType={'default'}
                 >
