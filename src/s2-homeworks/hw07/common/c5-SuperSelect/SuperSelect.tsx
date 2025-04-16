f73f94e463e5c3d7d26a8337862240735dc2abc4
Moi-glaza-vezde <moi.glaza.vezde@gmail.com>
2025-04-16 08:38:49 +0500

    hw12

--- src/s2-homeworks/hw07/common/c5-SuperSelect/SuperSelect.tsx
+++ src/s2-homeworks/hw07/common/c5-SuperSelect/SuperSelect.tsx
@@ -13,33 +13,42 @@ type DefaultSelectPropsType = DetailedHTMLProps<
 type SuperSelectPropsType = DefaultSelectPropsType & {
     options?: any[]
     onChangeOption?: (option: any) => void
+    change?: (id: number) => void
 }
 
 const SuperSelect: React.FC<SuperSelectPropsType> = ({
-    options,
-    className,
-    onChange,
-    onChangeOption,
-    ...restProps
-}) => {
+                                                         change,
+                                                         options,
+                                                         className,
+                                                         onChange,
+                                                         onChangeOption,
+                                                         ...restProps
+                                                     }) => {
     const mappedOptions: any[] = options
-        ? options.map((o) => (
-              <option
-                  id={'hw7-option-' + o.id}
-                  className={s.option}
-                  key={o.id}
-                  value={o.id}
-              >
-                  {o.value}
-              </option>
-          ))
+        ? options.map((o) => {
+
+            return (
+                <option
+                    id={'hw7-option-' + o.id}
+                    className={s.option}
+                    key={o.id}
+                    value={o.id}
+                >
+                    {o.value}
+                </option>
+            )
+        })
         : [] // map options with key
 
     const onChangeCallback = (e: ChangeEvent<HTMLSelectElement>) => {
         // делают студенты
+        const selectedId = Number(e.currentTarget.value);
+
         if (onChangeOption) {
             onChangeOption(e.currentTarget.value)
+
         }
+       change && change(selectedId)
     }
 
     const finalSelectClassName = s.select + (className ? ' ' + className : '')
--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,27 @@
 const initState = {
     themeId: 1,
 }
+type StateType = {
+    themeId: number,
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+}
+export const themeReducer = (state = initState, action: changeThemeIdActionType): StateType => { // fix any
     switch (action.type) {
         // дописать
+        case 'SET_THEME_ID':
+            return {
+                ...state,
+                themeId: action.id
+            }
 
         default:
             return state
     }
 }
+type changeThemeIdActionType = {
+    type: 'SET_THEME_ID',
+    id: number,
+}
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+
+export const changeThemeId = (id: number): changeThemeIdActionType => ({type: 'SET_THEME_ID', id}) // fix any
